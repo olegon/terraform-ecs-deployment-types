f051cfcdfddf98cd42da336d84ea70463f3ea58b
Leandro Gonçalves de Oliveira <olegon.main+github@gmail.com>
2025-12-09 18:54:53 -0300

    fix: removing deprecated '.name'

--- terraform-modules/app-module/aws-ecs-app.tf
+++ terraform-modules/app-module/aws-ecs-app.tf
@@ -17,7 +17,7 @@ resource "aws_ecs_task_definition" "app" {
         logDriver = "awslogs"
         options = {
           awslogs-group         = aws_cloudwatch_log_group.app.name
-          awslogs-region        = data.aws_region.current.name
+          awslogs-region        = data.aws_region.current.region
           awslogs-stream-prefix = "logs"
         }
       }
