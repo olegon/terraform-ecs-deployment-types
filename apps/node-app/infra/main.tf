350bc383fb27e066f5907973abcfbab081352fa7
Leandro Gonçalves de Oliveira <olegon.main+github@gmail.com>
2025-12-16 20:22:33 -0300

    feat: adding lambda for code deploy hooks

--- apps/node-app/infra/main.tf
+++ apps/node-app/infra/main.tf
@@ -29,3 +29,33 @@ module "infra" {
   deployment_bluegreen_strategy = "CodeDeployDefault.ECSAllAtOnce"
   deployment_type               = "Blue Green"
 }
+
+module "before_allow_traffic_lambda_hook" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = ">= 8, < 9"
+
+  function_name = "node-app-code-deploy-before-allow-traffic-lambda-hook"
+  handler       = "index.handler"
+  runtime       = "nodejs24.x"
+  publish       = true
+
+  timeout     = 300
+  memory_size = 128
+
+  source_path = [
+    "../before-allow-traffic-hook-lambda-src/index.js",
+  ]
+
+  # environment_variables = {
+  #   "X" = "Y"
+  # }
+
+  allowed_triggers = {
+    CodeDeployBeforeAllowTrafficHook = {
+      principal = "codedeploy.amazonaws.com"
+    }
+  }
+
+  cloudwatch_logs_retention_in_days = 7
+  cloudwatch_logs_log_group_class   = "INFREQUENT_ACCESS"
+}
