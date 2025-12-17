df65dd5e1e3b83086adf604472921873cd6fcfe9
Leandro Gonçalves de Oliveira <olegon.main+github@gmail.com>
2025-12-16 21:11:32 -0300

    feat: adding test to before allow traffic lambda hook

--- apps/node-app/before-allow-traffic-hook-lambda-src/index.mjs
+++ apps/node-app/before-allow-traffic-hook-lambda-src/index.mjs
@@ -8,17 +8,26 @@ export const handler = async (event, context) => {
     console.log('event = %s', JSON.stringify(event, null, 2));
     console.log('context = %s', JSON.stringify(context, null, 2));
 
-    /*
-     Enter validation tests here.
-    */
-
     const putLifecycleEventHookExecutionStatusCommand = new PutLifecycleEventHookExecutionStatusCommand({
         deploymentId: event.DeploymentId,
         lifecycleEventHookExecutionId: event.LifecycleEventHookExecutionId,
-        status: 'Succeeded'
+        status: await isTestSuccessful() ? 'Succeeded' : 'Failed'
     });
 
     console.log('putLifecycleEventHookExecutionStatusCommand = %s', JSON.stringify(putLifecycleEventHookExecutionStatusCommand, null, 2));
 
     await codedeploy.send(putLifecycleEventHookExecutionStatusCommand)
 };
+
+async function isTestSuccessful() {
+    try {
+        const testResponse = await fetch('http://my-lb-ingress-1855827051.us-east-1.elb.amazonaws.com:8080/node-app');
+        console.log('testResponse = %o', testResponse);
+
+        return testResponse.status === 200;
+    } catch (error) {
+        console.error('error = %o', error);
+
+        return false;
+    }
+}
