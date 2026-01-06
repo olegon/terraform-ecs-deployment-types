e3b36e8ac802c624df04d84d49798b20defeda21
Leandro Gonçalves de Oliveira <olegon.main+github@gmail.com>
2026-01-06 20:22:18 -0300

    fix: fixing CodeDeploy strategy name

--- apps/node-app/infra/main.tf
+++ apps/node-app/infra/main.tf
@@ -27,7 +27,7 @@ module "infra" {
   ecs_service_desired_count = 2
 
   # deployment_bluegreen_strategy = "CodeDeployDefault.ECSAllAtOnce"
-  deployment_bluegreen_strategy = "CodeDeployDefault.ECSCanary10percent5Minutes"
+  deployment_bluegreen_strategy = "CodeDeployDefault.ECSCanary10Percent5Minutes"
   deployment_type               = "Blue Green"
 }
 
